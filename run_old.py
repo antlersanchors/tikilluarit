011b8fbb4d3092fc39f7a30b51dc5190de7fbde0
Michael Liston <michaelowen.liston@gmail.com>
2015-11-20 00:43:11 +0100

    Let's try and get this going SMS style with Nexmo

--- run_old.py
+++ run_old.py
@@ -1,14 +1,11 @@
 from flask import Flask, request, redirect
-import nexmo
+import twilio.twiml
 from pb_py import main as api
 
 host = 'aiaas.pandorabots.com'
 user_key = 'ee3e0e5b6adedd58c40c4e2c84cb55ff'
 app_id = '1409612288159'
 botname = 'tiki'
-
-client = nexmo.Client(key='d8f52224', secret='2f64e1f8')
-
 
 app = Flask(__name__)
 
@@ -17,10 +14,7 @@ def bot_talk():
     """Respond to incoming texts with a text from your bot"""
     request_message = request.values.get('Body',  'twiliotest')
     bot_response = api.talk(user_key, app_id, host, botname, request_message)["response"]
-    # resp = twilio.twiml.Response()
-
-    client.send_message({'from': 'Python', 'to': '+4552602330', 'text': resp})
-    
+    resp = twilio.twiml.Response()
     resp.message(bot_response)
     return str(resp)
 
